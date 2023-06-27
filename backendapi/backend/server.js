ae99e1c5c9b27f1e68c4531d6ae00da69046cd41
Narender Singh <65656990+narender016@users.noreply.github.com>
2023-06-27 09:35:30 +0530

    Add files via upload

---
+++ backendapi/backend/server.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const app=express();
+
+//routes
+app.get('/',(req,res)=>{
+res.send("home page");
+})
+
+
+
+
+
+
+
+
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+})
